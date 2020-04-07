cfefec8b97430cfcd86b6d8e2308ac0493fdd7b7
HwangJaeHyuk <54526285+PoopooFaceMan@users.noreply.github.com>
2020-04-07 21:22:20 +0800

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+<title>Expense Tracker</title>
+</head>
+
+<body>
+
+<h1>Expense Tracker</h1>
+
+<p>
+<?php
+    echo "Hello\n";
+    echo "From the PHP Server\n";
+?>
+</p>
+
+</body>
+
+</html>
